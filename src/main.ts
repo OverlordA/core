30cfdcd95ed7e5ca3c79c640639f6b97080690ca
Anton <antonkotik0@gmail.com>
2023-07-06 12:34:12 +0300

    -  Added config module for provide ability use env variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const http_server_port = process.env.HTTP_SERVER_PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(http_server_port || 3000);
 }
-bootstrap();
+const startServerMessage = `servers started at port: ${http_server_port}`;
+
+bootstrap().then(() => {
+  // todo need to use outside logger to provide ability write logs in file not only console.
+  console.log(startServerMessage);
+});
